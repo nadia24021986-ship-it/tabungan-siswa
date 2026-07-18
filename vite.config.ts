3c580efbfaae42f8756afec5be8db0a28609c032
nadia24021986-ship-it <nadia24021986@gmail.com>
2026-07-18 11:02:59 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [
@@ -38,8 +39,12 @@ export default defineConfig({
       }
     })
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: {
     port: 5173
   }
 })
-
